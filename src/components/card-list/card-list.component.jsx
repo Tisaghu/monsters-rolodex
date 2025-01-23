7503d6904852a010631665386afd79bc2ee94897
Tisaghu <samasher9@hotmail.com>
2025-01-22 19:59:32 -0800

    CSS styling for searchbox and general App.css

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -7,7 +7,7 @@ class CardList extends Component {
 
 
         return(
-            <div>
+            <div className='card-list'>
                 {monsters.map(monster => (
                     <h1 key={monster.id}>{monster.name}</h1>
                 ))}
